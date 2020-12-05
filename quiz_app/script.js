c9458324692293efabe1fc511e93d8d343b24bb4
Ajith PG <ajithpg008@gmail.com>
2020-12-06 00:14:06 +0530

    UI Completed

---
+++ quiz_app/script.js
@@ -0,0 +1,19 @@
+const quizData = [
+    {
+        question: "When India got Independence?",
+        a:'1927',
+        b:'1930',
+        c:'1945',
+        d:'1900',
+        correct:'c'
+    },
+    {
+        question: "Which is the best js framework?",
+        a:'Angular',
+        b:'React',
+        c:'Vue',
+        d:'Spring',
+        correct:'b'
+    }
+
+]
