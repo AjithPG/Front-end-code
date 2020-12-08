7bffa5549b4a95a3e2dd6990927fce17cc574ff9
Ajith PG <ajithpg008@gmail.com>
2020-12-09 00:03:11 +0530

    Js completed

--- quiz_app/script.js
+++ quiz_app/script.js
@@ -16,4 +16,113 @@ const quizData = [
         correct:'b'
     }
 
-]
+];
+
+let currentQuestion = 0;
+let score = 0;
+
+const answerEls = document.querySelectorAll(".answer");
+
+const quiz = document.querySelector(".quiz_container");
+
+const currQuestion = document.getElementById('question');
+
+const a = document.getElementById('ans_a');
+const b = document.getElementById('ans_b');
+const c = document.getElementById('ans_c');
+const d = document.getElementById('ans_d');
+const submit_btn = document.getElementById('submit_btn');
+
+
+loadQuiz();
+
+function loadQuiz(){
+
+    answerDeselect();
+
+    const currentQuiz = quizData[currentQuestion];
+
+    
+
+    currQuestion.innerHTML = currentQuiz.question;
+    a.innerHTML = currentQuiz.a;
+    b.innerHTML = currentQuiz.b;
+    c.innerHTML = currentQuiz.c;
+    d.innerHTML = currentQuiz.d;
+
+ 
+
+}
+
+function gotselected(){
+
+ 
+
+    let answer;
+     
+    answerEls.forEach((answeEl)=>{
+
+        if(answeEl.checked){
+
+            answer =  answeEl.id;
+        }
+
+
+    });
+
+    return answer;
+
+}
+
+function answerDeselect(){
+
+    answerEls.forEach((answeEl)=>{
+
+        answeEl.checked = false;
+
+    });
+
+}
+
+
+submit_btn.addEventListener('click', () =>{
+
+
+    
+   const answer =  gotselected();
+
+   console.log(answer);
+
+  
+
+   if(answer){
+
+
+    if(answer === quizData[currentQuestion].correct){
+
+        score++;
+    }
+
+    currentQuestion++;
+    if(currentQuestion < quizData.length){
+
+        loadQuiz();
+       
+       }
+       else{
+
+           quiz.innerHTML = `<p class="result">You have answered ${score}/ ${quizData.length} Questions</p> <button class="submit" onclick=location.reload()>Reload</button>`
+          
+       }
+       
+      
+}
+
+
+    
+
+
+
+
+});
+
