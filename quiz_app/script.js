2b8e5dfd976e698ebc6b4a0e0af98b057fb5c7f7
Ajith PG <ajithpg008@gmail.com>
2020-12-09 20:27:06 +0530

    added new question and optimized code

--- quiz_app/script.js
+++ quiz_app/script.js
@@ -1,32 +1,60 @@
+
+//array to enter questions and answers
 const quizData = [
     {
-        question: "When India got Independence?",
-        a:'1927',
-        b:'1930',
-        c:'1945',
-        d:'1900',
+        question: "'OS' computer abbreviation usually means ?",
+        a:'Order of Significance',
+        b:'Open Software',
+        c:'Operating System',
+        d:'Optical Sensor',
         correct:'c'
     },
     {
-        question: "Which is the best js framework?",
-        a:'Angular',
-        b:'React',
-        c:'Vue',
-        d:'Spring',
+        question: "'.MOV' extension refers usually to what kind of file?",
+        a:'Image file',
+        b:'Animation/movie file',
+        c:'Audio file',
+        d:'MS Office document',
         correct:'b'
+    },
+
+    {
+        question: "A worldwide network of computers",
+        a:'CPU',
+        b:'Internet',
+        c:'RAM',
+        d:'Network',
+        correct:'b'
+    },
+
+    {
+        question: "What does acronym FOSS stand for?",
+        a:'Full Option Sensor System',
+        b:'Follow-On Support Service',
+        c:'Free and Open-Source Software',
+        d:'Fiber Optics Science System',
+        correct:'c'
+    },
+
+    {
+        question: "Which of the following is an important step towards the paperless concept?",
+        a:'Doxing',
+        b:'Digitizing',
+        c:'Debugging',
+        d:'Downloading',
+        correct:'d'
     }
 
 ];
 
+
+//variable initilization
 let currentQuestion = 0;
 let score = 0;
 
 const answerEls = document.querySelectorAll(".answer");
-
 const quiz = document.querySelector(".quiz_container");
-
 const currQuestion = document.getElementById('question');
-
 const a = document.getElementById('ans_a');
 const b = document.getElementById('ans_b');
 const c = document.getElementById('ans_c');
@@ -36,30 +64,25 @@ const submit_btn = document.getElementById('submit_btn');
 
 loadQuiz();
 
+
+//function to load questions from array
 function loadQuiz(){
 
     answerDeselect();
 
     const currentQuiz = quizData[currentQuestion];
-
-    
-
     currQuestion.innerHTML = currentQuiz.question;
     a.innerHTML = currentQuiz.a;
     b.innerHTML = currentQuiz.b;
     c.innerHTML = currentQuiz.c;
     d.innerHTML = currentQuiz.d;
-
- 
-
 }
 
+
+//function to get checked radio button value
 function gotselected(){
 
- 
-
-    let answer;
-     
+    let answer;     
     answerEls.forEach((answeEl)=>{
 
         if(answeEl.checked){
@@ -67,13 +90,13 @@ function gotselected(){
             answer =  answeEl.id;
         }
 
-
     });
 
     return answer;
 
 }
 
+// function to deseclect checked radio button
 function answerDeselect(){
 
     answerEls.forEach((answeEl)=>{
@@ -85,44 +108,29 @@ function answerDeselect(){
 }
 
 
+//click submit button
 submit_btn.addEventListener('click', () =>{
 
-
-    
    const answer =  gotselected();
-
-   console.log(answer);
-
-  
-
    if(answer){
 
-
     if(answer === quizData[currentQuestion].correct){
-
         score++;
     }
 
     currentQuestion++;
-    if(currentQuestion < quizData.length){
 
+    //check if the quiz is completed
+    if(currentQuestion < quizData.length){
         loadQuiz();
-       
        }
        else{
-
-           quiz.innerHTML = `<p class="result">You have answered ${score}/ ${quizData.length} Questions</p> <button class="submit" onclick=location.reload()>Reload</button>`
-          
+           quiz.innerHTML = `<p class="result">You have answered ${score}/ ${quizData.length} 
+           Questions</p> <button class="submit" onclick=location.reload()>Reload</button>`;
        }
        
       
 }
-
-
-    
-
-
-
 
 });
 
